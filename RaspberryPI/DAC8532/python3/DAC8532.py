f7002321553c477b1610a485194ee85efb03eb24
asssa776 <61865413+asssa776@users.noreply.github.com>
2020-03-06 09:22:41 +0100

    Update DAC8532.py
    #DAC_VREF = 3.3: if writing an output DAC with 5V ref it seems more convient to define that value in another script
    def DAC8532_Out_Voltage(self, Channel, Voltage, DAC_VREF): extending function with variable DAC_VREF allows to operate also woth 5V ref

--- RaspberryPI/DAC8532/python3/DAC8532.py
+++ RaspberryPI/DAC8532/python3/DAC8532.py
@@ -7,7 +7,7 @@ channel_B   = 0x34
 
 DAC_Value_MAX = 65535
 
-DAC_VREF = 3.3
+#DAC_VREF = 3.3
 
 class DAC8532:
     def __init__(self):
@@ -20,7 +20,7 @@ class DAC8532:
         config.spi_writebyte([Channel, Data >> 8, Data & 0xff])
         config.digital_write(self.cs_pin, GPIO.HIGH)#cs  0
         
-    def DAC8532_Out_Voltage(self, Channel, Voltage):
+    def DAC8532_Out_Voltage(self, Channel, Voltage, DAC_VREF):
         if((Voltage <= DAC_VREF) and (Voltage >= 0)):
             temp = int(Voltage * DAC_Value_MAX / DAC_VREF)
             self.DAC8532_Write_Data(Channel, temp)
